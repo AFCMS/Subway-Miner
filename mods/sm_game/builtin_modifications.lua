e1a9409db254e0059a007eb89589244d8844752c
AFCMS <afcm.contact@gmail.com>
2021-12-18 01:00:05 +0100

    make all nodes not pointable

--- mods/sm_game/builtin_modifications.lua
+++ mods/sm_game/builtin_modifications.lua
@@ -1,8 +1,14 @@
 minetest.register_on_mods_loaded(function()
+	--unregister all chatcommands except monitoring ones that are used for debugging
 	for name in pairs(minetest.registered_chatcommands) do
 		if name ~= "profiler" and name ~= "status" then
 			minetest.unregister_chatcommand(name)
 		end
+	end
+
+	--make all nodes not pointable to hide their borders ingame
+	for name in pairs(minetest.registered_nodes) do
+		minetest.override_item(name, {pointable = false, diggable = false})
 	end
 end)
 
